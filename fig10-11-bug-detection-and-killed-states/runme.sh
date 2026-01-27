0cc1fdf06e4d4cb1b4996472f6c08279c839d8f1
hassan <hassanaleem9@gmail.com>
2026-01-27 15:29:39 -0500

    code dump

--- fig10-11-bug-detection-and-killed-states/runme.sh
+++ fig10-11-bug-detection-and-killed-states/runme.sh
@@ -4,11 +4,11 @@ ulimit -s unlimited
 
 # clear old data
 
-# rm -rf json.h/*.pdf json.h/*.png
-# rm -rf libosip/*.pdf libosip/*.png libosip/killed_states.*
-# rm -rf libyaml/*.pdf libyaml/*.png libyaml/killed_states.*
-# rm -rf regex/*.pdf regex/*.png
-# rm -rf utf8.h/*.pdf utf8.h/*.png
+rm -rf json.h/*.pdf json.h/*.png
+rm -rf libosip/*.pdf libosip/*.png libosip/killed_states.*
+rm -rf libyaml/*.pdf libyaml/*.png libyaml/killed_states.*
+rm -rf regex/*.pdf regex/*.png
+rm -rf utf8.h/*.pdf utf8.h/*.png
 
 
 cd regex
@@ -17,30 +17,30 @@ mv *.pdf ../fig10a-regex.pdf
 mv *.png ../fig10a-regex.png
 cd ..
 
-# cd utf8.h
-# ./run.sh
-# mv *.pdf ../fig10b-utf8-h.pdf
-# mv *.png ../fig10b-utf8-h.png
-# cd ..
+cd utf8.h
+./run.sh
+mv *.pdf ../fig10b-utf8-h.pdf
+mv *.png ../fig10b-utf8-h.png
+cd ..
 
-# cd json.h
-# ./run.sh
-# mv *.pdf ../fig10c-json-h.pdf
-# mv *.png ../fig10c-json-h.png
-# cd ..
+cd json.h
+./run.sh
+mv *.pdf ../fig10c-json-h.pdf
+mv *.png ../fig10c-json-h.png
+cd ..
 
-# cd libosip
-# ./run.sh
-# mv *.pdf ../fig10d-libosip.pdf
-# mv *.png ../fig10d-libosip.png
-# cd ..
+cd libosip
+./run.sh
+mv *.pdf ../fig10d-libosip.pdf
+mv *.png ../fig10d-libosip.png
+cd ..
 
 
-# cd libyaml
-# ./run.sh
-# mv *.pdf ../fig10e-libyaml.pdf
-# mv *.png ../fig10e-libyaml.png
-# cd ..
+cd libyaml
+./run.sh
+mv *.pdf ../fig10e-libyaml.pdf
+mv *.png ../fig10e-libyaml.png
+cd ..
 
 # Kiled States
 python3 plot_killed_states.py libosip
